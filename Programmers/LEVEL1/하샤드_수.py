28bee41558b2334ff7c263b1d0c6f0d01c5e606e
kkkwp <chxrryda@gmail.com>
2022-07-14 19:25:58 +0900

    2022/07/14/Thu modified

--- Programmers/LEVEL1/하샤드_수.py
+++ Programmers/LEVEL1/하샤드_수.py
@@ -1,8 +1,7 @@
 # programmers, LEVEL1 : 하샤드 수
 def solution(x):
-    num = sum([int(c) for c in str(x)])
-    return True if x%num==0 else False
+    return x % sum([int(c) for c in str(x)]) == 0
 
 # 최적코드
 def solution(x):
-    return x%sum([int(c) for c in str(x)])==0
+    return x % sum(list(map(int, str(x)))) == 0
