0c006d0073d929ecadfd13a0ce03870d3e844576
chaeyun3 <67499154+kkkwp@users.noreply.github.com>
2024-04-12 12:12:20 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int binomial(int n, int k) {
+		if (k == 0 || n == k) {
+			return 1;
+		}
+		return binomial(n - 1, k - 1) + binomial(n - 1, k);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		System.out.println(binomial(n, k));
+	}
+}
