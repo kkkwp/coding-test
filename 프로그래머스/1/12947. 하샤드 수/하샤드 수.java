07233c91ec7aa06f79bc2062a15cca941b0f119b
chaeyun3 <67499154+kkkwp@users.noreply.github.com>
2024-07-13 01:28:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(int x) {
+        int tmp = x;
+        int sum = 0;
+        while (tmp > 0) {
+            sum += tmp % 10;
+            tmp /= 10;
+        }
+        return x % sum == 0;
+    }
+}
