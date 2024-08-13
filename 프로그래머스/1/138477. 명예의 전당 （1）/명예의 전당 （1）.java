f18a1d6eaf537b69f517c8f4295298f734cf75e3
chaeyun3 <67499154+kkkwp@users.noreply.github.com>
2024-08-13 14:24:05 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.51 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> hall = new PriorityQueue<>();
+        for (int i = 0; i < score.length; i++) {
+            hall.offer(score[i]);
+            if (hall.size() > k)
+                hall.poll();
+            answer[i] = hall.peek();
+        }
+        return answer;
+    }
+}
