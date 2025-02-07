449a747e42ffeb18bf512c85407414ab73e1ea3b
kkkwp <chxrryda@gmail.com>
2025-02-07 22:57:52 +0900

    [level 1] Title: 메리 크리스마스, Time: 69 ms, Memory: 9.93 MB

---
+++ 소프티어/1/9660. 메리 크리스마스/메리 크리스마스.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("##  ##     ##     #####    #####    ##  ##            ##  ##            ##   ##    ##      ####").append('\n');
+        sb.append("##  ##    ####    ##  ##   ##  ##   ##  ##            ##  ##            ### ###   ####    ##  ##").append('\n');
+        sb.append("##  ##   ##  ##   ##  ##   ##  ##   ##  ##             ####             #######  ##  ##   ##").append('\n');
+        sb.append("######   ######   #####    #####     ####               ##     ######   ## # ##  ######    ####").append('\n');
+        sb.append("##  ##   ##  ##   ##       ##         ##               ####             ##   ##  ##  ##       ##").append('\n');
+        sb.append("##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##   ##  ##").append('\n');
+        sb.append("##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##    ####");
+        System.out.print(sb.toString());
+    }
+}
