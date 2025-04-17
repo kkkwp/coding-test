86187fb20efa3b905b8701ab8e40f8d490d31032
chaeyun3 <chxrryda@gmail.com>
2025-04-17 17:28:30 +0900

    [level 1] Title: 과일 장수, Time: 59.93 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        int n = score.length;
+        Arrays.sort(score);
+        for (int i=n-m; i>=0; i-=m)
+            answer += score[i]*m;
+        return answer;
+    }
+}
