ddaf601d15c59c75f1db918dbc99d64613a0c04d
chaeyun3 <chxrryda@gmail.com>
2025-06-04 01:23:21 +0900

    [level 1] Title: 둘만의 암호, Time: 0.89 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i=0; i<n; i++) {
             char ch = s.charAt(i);
             for (int j=0; j<index; j++) {
-                ch += 1;
+                ch++;
                 if (ch > 'z')
                     ch -= 26;
                 if (skip.contains(String.valueOf(ch)))
