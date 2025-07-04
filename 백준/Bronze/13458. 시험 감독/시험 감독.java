cafd7ae286dc08721202bec1096de5cef91ee45b
yunny <chxrryda@gmail.com>
2025-07-04 21:28:58 +0900

    [Bronze II] Title: 시험 감독, Time: 416 ms, Memory: 92092 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -1,29 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] students = new int[n];
-		for (int i = 0; i < n; i++) {
-			students[i] = Integer.parseInt(st.nextToken());
-		}
-
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
 		int b = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 
 		long answer = n;
-		for (int student : students) {
-			student -= b;
-			if (student > 0) {
-				answer += (long)Math.ceil((double)student / c);
-			}
+		for (int x : a) {
+			x -= b;
+			if (x <= 0) continue;
+			answer += x / c;
+			if (x % c != 0) answer++;
 		}
 		System.out.println(answer);
 	}
