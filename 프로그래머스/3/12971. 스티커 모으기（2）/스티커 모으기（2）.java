58d179227290ff089c968916a3ce8d51a7dad635
yunny <chxrryda@gmail.com>
2025-07-27 00:38:25 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.65 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int sticker[]) {
+        int n = sticker.length;
+        
+        if (n == 1) return sticker[0];
+        if (n == 2) return Math.max(sticker[0], sticker[1]);
+        
+        // 첫 번째 스티커를 사용하는 경우 → 마지막 스티커는 사용 불가
+        int[] dp1 = new int[n];
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[0];
+        for (int i=2; i<n-1; i++)
+            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i]);
+        
+        // 첫 번째 스티커를 사용하지 않는 경우 → 마지막 스티커 사용 가능
+        int[] dp2 = new int[n];
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        for (int i=2; i<n; i++)
+            dp2[i] = Math.max(dp2[i-1], dp2[i-2] + sticker[i]);
+        
+        return Math.max(dp1[n-2], dp2[n-1]);
+    }
+}
