604a89b20fec7fb849817a687126d1e8b9628a2f
yunny <chxrryda@gmail.com>
2025-08-27 16:23:01 +0900

    [level 2] Title: 구명보트, Time: 10.35 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,19 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int n = people.length;
         Arrays.sort(people);
         
-        int left = 0, right = n-1;
+        int left = 0, right = people.length-1;
         while (left <= right) {
-            if (people[left] + people[right] <= limit) {
-                answer++;
+            answer++;
+            if (people[left] + people[right] <= limit)
                 left++;
-                right--;
-            } else {
-                right--;
-                answer++;
-            }
+            right--;
         }
         return answer;
     }
