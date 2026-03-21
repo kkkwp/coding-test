8012f7fe6aff954c00a7ba2633f8080cc0fe1ec2
yunny 3 <chxrryda@gmail.com>
2026-03-21 19:06:35 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 19732 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,64 +9,62 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static boolean[] visited;
-    static List<Integer>[] graph;
+	static List<List<Integer>> adj;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); // 정점
-        m = Integer.parseInt(st.nextToken()); // 간선
-        int v = Integer.parseInt(st.nextToken()); // 시작점
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()); // 정점의 개수 n
+		int m = Integer.parseInt(st.nextToken()); // 간선의 개수 m
+		int v = Integer.parseInt(st.nextToken()); // 탐색을 시작할 정점의 번호 v
 
-        // 그래프 초기화
-        graph = new ArrayList[n+1];
-        for (int i=1; i<=n; i++)
-            graph[i] = new ArrayList<>();
-        for (int i=0; i<m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
-        }
+		// 인접리스트 초기화 (처음부터 n번까지)
+		adj = new ArrayList<>();
+		for (int i=0; i<=n; i++)
+			adj.add(new ArrayList<>());
+		for (int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			adj.get(from).add(to);
+			adj.get(to).add(from);
+		}
 
-        // 정점 번호가 작은 것부터 방문하도록 정렬
-        for (int i=1; i<=n; i++)
-            Collections.sort(graph[i]);
+		// 번호 작은 순으로 정렬
+		for (int i=1; i<=n; i++)
+			Collections.sort(adj.get(i));
 
-        StringBuilder sb = new StringBuilder();
-        visited = new boolean[n+1];
-        dfs(v, sb);
-        sb.append("\n");
-        visited = new boolean[n+1];
-        bfs(v, sb);
-        System.out.println(sb);
-    }
+		visited = new boolean[n+1];
+		dfs(v);
+		sb.append('\n');
+		visited = new boolean[n+1];
+		bfs(v);
+		System.out.println(sb);
+	}
 
-    static void dfs(int cur, StringBuilder sb) {
-        visited[cur] = true;
-        sb.append(cur).append(" ");
-        for (int next : graph[cur])
-            if (!visited[next])
-                dfs(next, sb);
-    }
+	static void dfs(int v) {
+		visited[v] = true;
+		sb.append(v).append(' ');
+		for (int next : adj.get(v))
+			if (!visited[next])
+				dfs(next);
+	}
 
-    static void bfs(int start, StringBuilder sb) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visited[start] = true;
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            sb.append(cur).append(" ");
-            for (int next : graph[cur]) {
-                if (!visited[next]) {
-                    q.add(next);
-                    visited[next] = true;
-                }
-            }
-        }
-    }
+	static void bfs(int v) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(v);
+		visited[v] = true;
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			sb.append(cur).append(' ');
+			for (int next : adj.get(cur)) {
+				if (!visited[next]) {
+					q.add(next);
+					visited[next] = true;
+				}
+			}
+		}
+	}
 }
